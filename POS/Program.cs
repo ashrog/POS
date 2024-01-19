10591e0a7523c03d106aea7c134f903c3098dc08
ashrog <yohannesabel398@gmail.com>
2024-01-19 20:18:56 +0300

    login first

--- POS/Program.cs
+++ POS/Program.cs
@@ -11,7 +11,7 @@ namespace POS
             // To customize application configuration such as set high DPI settings or default font,
             
             ApplicationConfiguration.Initialize();
-            Application.Run(new Billings());
+            Application.Run(new Login());
         }
     }
 }
